aae7d3a84e6e71756fcf894c27d7ccc4bdc9f544
devraj4522 <55313450+devraj4522@users.noreply.github.com>
2020-08-23 14:25:28 +0530

    Addev Via Upload  CalculateTheNthTerm- Recursion.c

---
+++ SomeChallangingCProgram/CalculateTheNthTerm- Recursion.c
@@ -0,0 +1,57 @@
+// ****** Sample Input 0 ********
+
+// 5
+// 1 2 3
+
+// ******* Sample Output 0 ********
+
+// 11
+
+// ******* Explanation 0 ******
+// S(1)  = 1
+// S(2) =2
+// S(3) =3
+//  S(4) = S(1) + S(2)  + S(3)
+// Consider the following steps:
+
+// ******** Program ******** 
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//Complete the following function.
+
+int find_nth_term(int n, int a, int b, int c) {
+  //Write your code here.
+  if (n ==1)
+  {
+      return a;
+  }
+  else if (n==2)
+  {
+      return b;
+  }
+  else if (n==3)
+  {
+      return c;
+  }
+  else
+  {
+      return (find_nth_term(n-1, a, b, c) + find_nth_term(n-2, a, b, c) + find_nth_term(n-3, a, b, c));
+  }
+  
+  
+  
+  
+}
+
+int main() {
+    int n, a, b, c;
+  
+    scanf("%d %d %d %d", &n, &a, &b, &c);
+    int ans = find_nth_term(n, a, b, c);
+ 
+    printf("%d", ans); 
+    return 0;
+}
